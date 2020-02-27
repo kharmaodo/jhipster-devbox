4b98d2c2291db9ff303d2b971d7f4b3f215ba190
Maodo DIOP <dmaodo@gmail.com>
2020-02-27 15:41:07 +0000

    Adding Provisiong script shell for Nginx

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -200,7 +200,21 @@ PREFS
 
 echo "Install  SDK for JVM microservice like quarkus,micronaut,thorntail"
 echo "Install  Nginx "
-echo "Install  Nginx "
+# nginx
+sudo apt-get -y install nginx
+sudo service nginx start
+
+# set up nginx server
+sudo cp /vagrant/.provision/nginx/nginx.conf /etc/nginx/sites-available/site.conf
+sudo chmod 644 /etc/nginx/sites-available/site.conf
+sudo ln -s /etc/nginx/sites-available/site.conf /etc/nginx/sites-enabled/site.conf
+sudo service nginx restart
+
+# clean /var/www
+sudo rm -Rf /var/www
+
+# symlink /var/www => /vagrant
+ln -s /vagrant /var/www
 echo "Install  Sonar"
 echo "Install  CouchDB"
 echo "Install  Eureka"
